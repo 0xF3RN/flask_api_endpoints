9b02aa663ee555177096db4b97055f094e109dc6
Eugene <evgeniyzhukowskiy@gmail.com>
2023-08-25 13:38:39 +0300

    client_test

--- client_test/client.py
+++ client_test/client.py
@@ -1,25 +1,29 @@
 import requests
 import json
 
-url1 = "http://127.0.0.1:5000/api/add"
-url = 'http://127.0.0.1:5000/api/get'
+url_post = "http://127.0.0.1:5000/api/add"
+url_get = 'http://127.0.0.1:5000/api/get'
+
 
 def send_json(json_file):
     with open(json_file, "r") as file:
         send_data = json.load(file)
-        response = requests.post(url1, json=send_data)
+        response = requests.post(url_post, json=send_data)
         print(response.text)
 
 
 def get_data():
-    response = requests.get(url)
+    params = {
+        "malwareList": ["Lockbit"]
+    }
+    response = requests.get(url_get, params=params)
     print(response.json())
 
 
 def main():
-    get_data()
-    #send_json("valid_json.json")
-    #send_json("valid_json_x2.json")
+    get_data()                                           # gets data based on params
+    #send_json("valid_json.json")                        # adds 1 json
+    #send_json("valid_json_x2.json")                     # adds 2 jsons
     #send_json("not_valid_json_no_items_field.json")     # without items field
     #("not_valid_json_empty_fields.json")                # empty fields
 
