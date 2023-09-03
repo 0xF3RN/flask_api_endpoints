a7f2b0a5a5058627d1b85d4106714cab55c6344b
Eugene <evgeniyzhukowskiy@gmail.com>
2023-09-03 16:59:36 +0300

    Json Parser
    added: recursive json parse

--- JsonParser.py
+++ JsonParser.py
@@ -1,23 +1,31 @@
-import json
+import sys
 
 
 class JSONParser:
     def __init__(self, json_data):
         self.data = json_data
-        print("-> parser has been initialized")
-
-    def print_data(self):
-        return self.data
+        sys.setrecursionlimit(2000)
+        print(f"-> parser has been initialized, recursion limit is {sys.getrecursionlimit()}")
 
     def __del__(self):
         print("-> parser has been deleted")
 
-    def json_validation(self):
-        validation_list = ('author', 'companyId', 'id', "indicators", "indicatorsIds", "isPublished",
-                           "isTailored", "labels", "langs", "malwareList", "seqUpdate")
-        for item in self.data:
-            data_validation = all(item.get(key) is not None and item.get(key) != [] and
-                                  item.get(key) != '' for key in validation_list)
-            if data_validation:
-                return True
-        return False
+    def json_validation(self, data=None):
+        if data is None:
+            data = self.data
+        if isinstance(data, dict):
+            if not data:
+                return False
+            for key, value in data.items():
+                if value is None or (isinstance(value, str) and value.strip() == ""):
+                    return False
+                elif isinstance(value, (dict, list)):
+                    if not self.json_validation(value):
+                        return False
+        elif isinstance(data, list):
+            if not data:
+                return False
+            for item in data:
+                if not self.json_validation(item):
+                    return False
+        return True
